b28fc839f4d95bd42742535c81086ac570fd1ce7
Juan Adonis Núñez Collado <adn221294@gmail.com>
2022-07-26 23:48:22 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -19,14 +19,29 @@ from django.urls import path, include
 from gestion_vehiculos_api.views import * #Importa todas las vistas disponibles.
 
 
-#----------------------------------------------------------------------
+#--------------------------------URLs--------------------------------------
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),  
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    
+    #URLs para choferes:
     path('get_choferes/<str:id>', get_choferes),
     path('post_chofer', post_chofer),
+    path('put_chofer/<int:id>', put_chofer),
+    path('delete_chofer/<int:id>', delete_chofer),
+    
+    #URLs para vehiculos:
+    path('get_vehiculos/<str:id>', get_vehiculos),
+    path('post_vehiculo', post_vehiculo),
+    path('put_vehiculo/<int:id>', put_vehiculo),
+    path('delete_vehiculo/<int:id>', delete_vehiculo),
 
+    #URLs para viajes:
+    path('get_viajes/<str:id>', get_viajes),
 ]
+
 
 
 
