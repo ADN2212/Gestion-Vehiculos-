e20f019b1bdae74cfd8853d916ecf0032028ea6b
Juan Adonis Núñez Collado <adn221294@gmail.com>
2022-07-26 23:51:02 -0400

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -1,7 +1,6 @@
 from gestion_vehiculos_api.models import Chofer, Vehiculo, Viaje, GVUser
 from rest_framework import serializers
 
-
 """
 Esta parte define como que informacion de los modelo se mostrará en la respuesta (responce)
 que dará la API cuando se le haga una peticion (request).  
@@ -19,23 +18,44 @@ class ChoferSerializer(serializers.HyperlinkedModelSerializer):
         			'nivel_escolar', 'sexo', 'agnos_experiencia', 
         			'cantidad_de_multas', 'clasificacion']
 
+    #Si ejecuto este metdo antes de usar .save() optendré un AssertionError. 
+	"""
 	def validar_edad(self):
 		return self.data['edad'] < 18
-
-
-
-
+	"""
 
 class VehiculoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Vehiculo
-        fields = ['marca', 'placa']
+        fields =	[	'id_vehiculo', 'placa', 'marca', 
+        				'peso_en_toneladas', 'tipo_de_comnustible', 'kms_recorridos',
+        				'velocidad_maxima', 'agnos_explotacion', 'tipo_servicio',
+        				'climatizacion', 'cantidad_asientos', 'disponibilidad',
+        			]
+        
+        #fields = '__all__'
 
 class ViajeSerializer(serializers.HyperlinkedModelSerializer):
+	
+	chofer = ChoferSerializer()
+	vehiculo = VehiculoSerializer()
+
+	#print(chofer)
+
+
 	class Meta:
 		model = Viaje
-		#fields = ['chofer', 'inicio_viaje', 'fin_viaje', 'costo_usd']
-		fields = '__all__'
+		"""
+		fields =	[	'id_viaje', 'tipo_viaje', 'inicio_viaje', 
+						'fin_viaje', 'costo_usd', 'kms_recorridos',
+						'comentarios', 'vehiculo', 'chofer',
+						'estado_viaje',
+					]
+		"""	
+		#fields = '__all__'
+
+		fields =	['id_viaje', 'tipo_viaje', 'inicio_viaje', 'fin_viaje', 'costo_usd', 'kms_recorridos', 'comentarios', 'estado_viaje', 'chofer', 'vehiculo']
+
 
 class GVUserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
@@ -44,6 +64,11 @@ class GVUserSerializer(serializers.HyperlinkedModelSerializer):
 						'numero_telefono', 'direccion_particular',
 						'email'
 					]
+
+
+
+
+
 
 
 
