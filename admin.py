ceb268c42ea0f0322e92df4fe00a3a8714f70bc7
Juan Adonis Núñez Collado <adn221294@gmail.com>
2022-08-11 10:08:43 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from gestion_vehiculos_api.models import Vehiculo, Viaje, Chofer, GVUser
+from gestion_vehiculos_api.models import Vehiculo, Viaje, Chofer#, GVUser
 
 class Administrador_vehiculos(admin.ModelAdmin):
 	list_display = ('id_vehiculo', 'marca', 'placa', 'tipo_servicio')
@@ -10,7 +10,7 @@ class Administrador_vehiculos(admin.ModelAdmin):
 admin.site.register(Vehiculo, Administrador_vehiculos)
 admin.site.register(Viaje)
 admin.site.register(Chofer)
-admin.site.register(GVUser)
+#admin.site.register(GVUser)
 
 
 
